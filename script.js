ff9a1f228565dde8f96d0d57368eb9bcff867f58
MostlyK <135974627+MostlyKIGuess@users.noreply.github.com>
2023-10-29 04:31:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const optionsContainer = document.getElementById("options-container");
 
 let currentQuestionIndex = 0;
 
+// Initialize accumulatedScores array to keep track of personality trait scores
+const accumulatedScores = Array(9).fill(0);
+
 function showNextQuestion() {
     if (currentQuestionIndex < quizData.length) {
         const questionData = quizData[currentQuestionIndex];
@@ -15,27 +18,11 @@ function showNextQuestion() {
     }
 }
 
-function updateScores(score) {
-    for (let i = 0; i < scores.length; i++) {
-        scores[i] += score * quizData[currentQuestionIndex].options[i];
+// Function to update the accumulated scores after each question
+function updateAccumulatedScores(score) {
+    for (let i = 0; i < accumulatedScores.length; i++) {
+        accumulatedScores[i] += quizData[currentQuestionIndex].options[i] * score;
     }
-}
-
-function displayResult() {
-    // Calculate the scores for each personality
-    const scores = Array(9).fill(0);
-
-    for (let i = 0; i < quizData.length; i++) {
-        const selectedOption = document.querySelector('input[name="q"]:checked');
-        if (selectedOption) {
-            const score = parseFloat(selectedOption.value);
-            updateScores(score);
-        }
-    }
-
-    // Redirect to the results page and pass the scores as query parameters
-    const url = `results.html?${scores.map((score, index) => `p${index}=${score}`).join('&')}`;
-    window.location.href = url;
 }
 
 form.addEventListener("submit", (e) => {
@@ -43,15 +30,11 @@ form.addEventListener("submit", (e) => {
     const selectedOption = document.querySelector('input[name="q"]:checked');
     if (selectedOption) {
         const score = parseFloat(selectedOption.value);
-        updateScores(score);
+        updateAccumulatedScores(score); // Update the accumulated scores based on user's response
     }
     currentQuestionIndex++;
     showNextQuestion();
+    if (currentQuestionIndex === quizData.length) {
+        displayResult(); // Display result after the last question
+    }
 });
-
-nextButton.addEventListener("click", () => {
-    currentQuestionIndex++;
-    showNextQuestion();
-});
-
-showNextQuestion();
